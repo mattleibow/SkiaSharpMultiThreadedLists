f3c6c77df33b656c7c7a1769a5c6088176b222ab
Valeriu Balan <balan.valeriu@live.com>
2019-01-02 11:13:30 +0200

    feat: Added UWP project.

---
+++ MultiThreadedLists.UWP/MainPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Windows.UI.Xaml.Controls;
+using Xamarin.Forms.Platform.UWP;
+
+namespace MultiThreadedLists.UWP
+{
+    public sealed partial class MainPage : WindowsPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            LoadApplication(new MultiThreadedLists.App());
+        }
+    }
+}
